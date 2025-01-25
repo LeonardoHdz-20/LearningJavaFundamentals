f102b4665a57e652bf9ad89329bdb97b78d522f1
LeonardoHdz-20 <134025927+LeonardoHdz-20@users.noreply.github.com>
2025-01-25 01:25:02 -0600

    Create inputs.java

---
+++ src/main/java/edu/utvt/clase1/inputs.java
@@ -0,0 +1,25 @@
+package edu.utvt.clase1;
+
+import java.util.Scanner;
+
+public class inputs {
+
+    public static void main(String[] args) {
+
+        String name;
+        int age;
+        String message = "Hola %s, tu edad es %s";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el nombre: ");
+        name = sc.nextLine();
+
+        System.out.print("Ingrese el edad: ");
+        age = sc.nextInt();
+
+        sc.close();
+        String finalMessage = String.format(message, name, age);
+        System.out.println(finalMessage);
+    }
+
+}
