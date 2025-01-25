dd73ac1c4e8ce682cd4ea9b7feabf16e8f56dba0
LeonardoHdz-20 <134025927+LeonardoHdz-20@users.noreply.github.com>
2025-01-25 01:20:12 -0600

    Create ElseIfStatement.java

---
+++ src/main/java/edu/utvt/clase1/ElseIfStatement.java
@@ -0,0 +1,20 @@
+package edu.utvt.clase1;
+
+public class ElseIfStatement {
+
+
+    public static void main(String[] args) {
+
+        int age = 56;
+
+        if (age <= 12)
+            System.out.println("Eres un niño");
+        else if (age < 17)
+            System.out.println("Eres un adolescente");
+        else if (age < 35)
+            System.out.println("Eres un adulto joven");
+        else
+            System.out.println("Eres un adulto");
+
+    }
+}
